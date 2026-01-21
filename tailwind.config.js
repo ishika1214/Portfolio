4cde75923e090a4c4dd0fc4a66b400111f30543d
ishika1214 <kumariishika874@gmail.com>
2026-01-22 00:36:20 +0530

    added animation wave

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    fontFamily:{
-      signature:["Great Vibes"],
+    extend: {
+      keyframes: {
+        wave: {
+          "0%": { transform: "rotate(0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10deg)" },
+          "60%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(0deg)" },
+        },
+      },
+      animation: {
+        wave: "wave 1.5s ease-in-out infinite",
+      },
+    },
+    fontFamily: {
+      signature: ["Great Vibes"],
     },
   },
   plugins: [],
-}
-
+};
